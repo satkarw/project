c9ddf9965fb28ba297bd4d7edf87390d564436fd
satkarw <satkarw@gmail.com>
2024-08-21 11:46:32 +0545

    Fix Vite configuration to resolve Firebase module import issues

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['firebase/auth', 'firebase/firestore', 'firebase/database'],
+
+      external: [],
     },
   },
 });
